b946ff9609750246f1376d2f13278eb67450532e
desmigor <mporeigor@gmail.com>
2022-10-06 11:14:21 +0300

    Implement miss_shader, image clearing, calling ray_generation, and saving in ray_tracing_renderer class

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -180,7 +180,8 @@ namespace cg::renderer
 				ray ray(position, ray_direction);
 
 				payload payload = trace_ray(ray, depth);
-				render_target->item(x, y) = payload.color;
+				render_target->item(x, y) = unsigned_color::from_color(
+						payload.color) ;
 			}
 		}
 		// TODO Lab: 2.06 Implement TAA in `ray_generation` method of `raytracer` class
